3445789770ec59073f57682b556880e53d80e5a8
kanziw <kanziwoong@gmail.com>
2022-05-06 01:22:11 +0900

    Revert "[TMP] Fix ddl.sql"
    This reverts commit 604ebc695eb1b7fb2357f5e33f1c1a3e39f6819d.

--- ddl.sql
+++ ddl.sql
@@ -5,7 +5,7 @@
 -- DATABASE `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 -- USE
--- `test`
+-- `test`;
 
 CREATE TABLE `user`
 (
